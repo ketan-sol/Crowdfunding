164d78ccfaa66e3193a9919c224eadf0d76f9be2
ketan-sol <ketam@solulab.com>
2022-07-12 19:37:45 +0530

    updated smart contract

--- contracts/Crowdfunding.sol
+++ contracts/Crowdfunding.sol
@@ -13,6 +13,18 @@ contract Crowdfunding {
 
     mapping(address => uint256) public contributors;
 
+    struct Request {
+        string description;
+        address payable recipient;
+        uint256 value;
+        bool completed;
+        uint256 numberOfVoters;
+        mapping(address => bool) voters;
+    }
+
+    mapping(uint256 => Request) public requests;
+    uint256 public numberOfRequests;
+
     constructor(
         uint256 _target,
         uint256 _deadline,
@@ -26,10 +38,7 @@ contract Crowdfunding {
 
     function sendEth() public payable {
         require(block.timestamp < deadline, "Deadline has passed");
-        require(
-            msg.value >= minContribution,
-            "Minimum contribution not satisfied"
-        );
+        require(msg.value >= minContribution, "Minimum contribution not met");
 
         if (contributors[msg.sender] == 0) {
             numberOfContributors++;
@@ -40,5 +49,64 @@ contract Crowdfunding {
 
     function getContractBalance() public view returns (uint256) {
         return address(this).balance;
+    }
+
+    function refund() public {
+        require(
+            block.timestamp > deadline && raisedAmount < target,
+            "You are not eligible for refund"
+        );
+        require(
+            contributors[msg.sender] > 0,
+            "You are not eligible for refund"
+        );
+        address payable user = payable(msg.sender);
+        user.transfer(contributors[msg.sender]);
+        contributors[msg.sender] = 0;
+    }
+
+    modifier onlyManager() {
+        require(msg.sender == manager, "Only manager can call this function");
+        _;
+    }
+
+    function createRequest(
+        string memory _description,
+        address payable _recipient,
+        uint256 _value
+    ) public onlyManager {
+        Request storage newRequest = requests[numberOfRequests];
+        numberOfRequests++;
+        newRequest.description = _description;
+        newRequest.recipient = _recipient;
+        newRequest.value = _value;
+        newRequest.completed = false;
+        newRequest.numberOfVoters = 0;
+    }
+
+    function voting(uint256 requestNumber) public {
+        require(contributors[msg.sender] > 0, "You must be a contributor");
+        Request storage currentRequest = requests[requestNumber];
+        require(
+            currentRequest.voters[msg.sender] == false,
+            "You have already voted"
+        );
+        currentRequest.voters[msg.sender] = true;
+        currentRequest.numberOfVoters++;
+    }
+
+    function Payment(uint256 requestNumber) public onlyManager {
+        require(raisedAmount >= target, "Insufficient funds");
+        Request storage currentRequest = requests[requestNumber];
+        require(
+            currentRequest.completed == false,
+            "The request has already been completed"
+        );
+        require(
+            currentRequest.numberOfVoters > numberOfContributors / 2,
+            "no majority"
+        );
+        currentRequest.recipient.transfer(currentRequest.value);
+        currentRequest.completed = true;
     }
 }
